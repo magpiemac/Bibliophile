2c09b47a35b6e1344c7de494f867a8f803df3387
github username <github email address>
2018-06-07 12:43:57 -0400

    Removed search option from navbar.

--- bibliophile-client/src/components/Navigation.js
+++ bibliophile-client/src/components/Navigation.js
@@ -33,9 +33,7 @@ class Navigation extends React.Component {
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
-          <entry className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </entry>
+          
         </form>
       </div>
     </nav>
