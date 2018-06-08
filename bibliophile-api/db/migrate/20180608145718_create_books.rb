24f1fde61318c29df45b7c0c975e9f8a097050c7
github username <github email address>
2018-06-08 11:05:45 -0400

    Ran scaffold for books and created books database.

---
+++ bibliophile-api/db/migrate/20180608145718_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :author
+      t.text :notes
+      t.string :status
+      t.string :img_url
+
+      t.timestamps
+    end
+  end
+end
