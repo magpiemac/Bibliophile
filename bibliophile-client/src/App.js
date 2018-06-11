993fe8dba79a5f525a6aac6f9c349410109f4939
github username <github email address>
2018-06-11 13:00:38 -0400

    Added home.js, and css files, updated navbar to custom navbar and tweaked code.

--- bibliophile-client/src/App.js
+++ bibliophile-client/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Navigation from './components/Navigation';
+import CustomNavbar from './components/CustomNavbar';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navigation />
+        <CustomNavbar />
         <div className="container">
           <h1>Bibliophile: the Obsessive Book Lover</h1>
           <img className = "" src="http://cdn01.wallconvert.com/_media/wallpapers_1920x1080/1/1/old-library-4190.jpg" alt="book" style={{ width:'100%' }}/>
---
+++ bibliophile-client/src/components/Home.js
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import { Grid, Row, Col, Image, Button } from 'react-bootstrap';
+import './Home.css';
