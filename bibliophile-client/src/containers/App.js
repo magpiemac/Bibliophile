262e3216c90b029ad6361904954eed80e6673353
github username <github email address>
2018-06-11 16:00:50 -0400

    Connected React app to api, added .env file.

--- bibliophile-client/src/containers/App.js
+++ bibliophile-client/src/containers/App.js
@@ -3,25 +3,24 @@ import './App.css';
 import Books from './Books';
 import CustomNavbar from '../components/CustomNavbar';
 
-const books = [
-  {
-    title: "A New Hope",
-    author: "Shannon McCormack",
-    notes: "Great read.",
-    status: "Done",
-    img_url: "https://books.google.com/books/content?id=i3GGvgAACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70w9faY3xBbKtdOVDIWQt7A46iSgZOLeVQBVegwSVMc_xa2Iwiou1ScbYiSaDOlr-jbFqVrgzgdI2QVskJ8BNxtTvZXBUYDEBlwPBG5f5IgprVXu-TFw3-dTzcjKtS6yGYlHuC3"
-  },
-  {
-    title: "A New Hope",
-    author: "Shannon McCormack",
-    notes: "Great read.",
-    status: "Done",
-    img_url: "https://books.google.com/books/content?id=i3GGvgAACAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70w9faY3xBbKtdOVDIWQt7A46iSgZOLeVQBVegwSVMc_xa2Iwiou1ScbYiSaDOlr-jbFqVrgzgdI2QVskJ8BNxtTvZXBUYDEBlwPBG5f5IgprVXu-TFw3-dTzcjKtS6yGYlHuC3"
-  }
-]
-
+const API_URL = process.env.REACT_APP_API_URL;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('${API_URL}/books')
+    .then(response => response.json())
+    .then(books => this.setState({ books }))
+  }
+
   render() {
     return (
       <div>
@@ -29,7 +28,7 @@ class App extends Component {
         <div className="container">
           <h1>Bibliophile: for the Obsessive Book Lover</h1>
           <div className="App">
-            <Books books={books} />
+            <Books books={this.state.books} />
           </div>
        </div>
      </div>
--- bibliophile-client/src/containers/Books.js
+++ bibliophile-client/src/containers/Books.js
@@ -1,17 +1,17 @@
 import React from 'react';
-// import './App.css';
+//import './App.css';
 import './Books.css';
 
 const Books = (props) => (
-  <div>
+  <div className="BooksContainer">
     <h3>Books Component</h3>
     {props.books.map(book =>
-      <div>
+      <div key={book.id} className="BookCard">
       <h3>{book.title} </h3>
       <p>Author: {book.author}</p>
       <p>Notes: {book.notes}</p>
       <p>Status: {book.status}</p>
-      <img src={book.img_url} alt={book.name} />
+      <img className="BookImage" src={book.img_url} alt={book.name} />
       </div>
     )}
   </div>
