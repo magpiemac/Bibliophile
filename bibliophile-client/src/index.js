e1f14462858d0f15c7ec13c279fd8a646f376ea1
github username <github email address>
2018-06-12 14:40:09 -0400

    Added store.js file, installed redux and thunk, added basic store and reducer to file.

--- bibliophile-client/src/index.js
+++ bibliophile-client/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store.js';
 
+console.log(store);
 
 ReactDOM.render(
   <App />,
