9673cc4401281c9cb8ec039c3558ddecf5dda120
github username <github email address>
2018-06-13 12:46:45 -0400

    Added event handler to BookForm.js, created new file in actions/bookForm.js.

--- bibliophile-client/src/containers/BookForm.js
+++ bibliophile-client/src/containers/BookForm.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { updateBookFormData } from '../actions/bookForm'
 
 class BookForm extends Component {
+
+  handleOnChange = event => {
+    const { name, value } = event.target;
+    const currentBookFormData = Object.assign({}, this.props.bookFormData, {
+      [name]: value
+    })
+    this.props.updateBookFormData(currentBookFormData)
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault()
+    this.props.createBook(this.props.bookFormData)
+  }
 
   render() {
     const { title, author, notes, status, img_url } = this.props.bookFormData;
@@ -9,37 +23,51 @@ class BookForm extends Component {
       <div>
         Add a Book
         <form>
-          <label htmlFor='title'>Title:</label>
-          <input
-            type='text'
-            name='title'
-            value={title}
-          />
-
-          <label htmlFor='author'>Author:</label>
-          <input
-            type='text'
-            name='author'
-            value={author}
-          />
-          <label htmlFor='notes'>Notes:</label>
-          <input
-            type='text'
-            name='notes'
-            value={notes}
-          />
-          <label htmlFor='status'>Status:</label>
-          <input
-            type='text'
-            name='status'
-            value={status}
-          />
-          <label htmlFor='img_url'>Cover Image:</label>
-          <input
-            type='text'
-            name='img_url'
-            value={img_url}
-          />
+          <div>
+            <label htmlFor='title'>Title:</label>
+            <input
+              type='text'
+              onChange={this.handleOnChange}
+              name='title'
+              value={title}
+            />
+          </div>
+          <div>
+            <label htmlFor='author'>Author:</label>
+            <input
+              type='text'
+              onChange={this.handleOnChange}
+              name='author'
+              value={author}
+            />
+          </div>
+          <div>
+            <label htmlFor='notes'>Notes:</label>
+            <input
+              type='text'
+              onChange={this.handleOnChange}
+              name='notes'
+              value={notes}
+            />
+          </div>
+          <div>
+            <label htmlFor='status'>Status:</label>
+            <input
+              type='text'
+              onChange={this.handleOnChange}
+              name='status'
+              value={status}
+            />
+          </div>
+          <div>
+            <label htmlFor='img_url'>Cover Image:</label>
+            <input
+              type='text'
+              onChange={this.handleOnChange}
+              name='img_url'
+              value={img_url}
+            />
+          </div>
         </form>
       </div>
     )
