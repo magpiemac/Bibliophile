182e47dbcc56b095c892be4dc26c2774dc8a366d
github username <github email address>
2018-06-16 12:08:13 -0400

    Added placeholder buttons for delete and edit in BookCard.

--- bibliophile-client/src/components/BookCard.js
+++ bibliophile-client/src/components/BookCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const BookCard = ({ book }) => (
   <div key={book.id} className="BookCard">
   <h3>{book.title} </h3>
-  <p><button type="delete">Remove Book</button></p>
+  <button type="delete">Delete Book</button>
   <button type="edit">Edit Book</button>
   <p>Author: {book.author}</p>
   <p>Notes: {book.notes}</p>
