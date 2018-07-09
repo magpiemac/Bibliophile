3e652b48d1dbe7e76e8422247d8c238162b69134
github username <github email address>
2018-07-09 13:50:01 -0400

    Removed test BookEdit code.

--- bibliophile-client/src/components/BookEdit.js
+++ bibliophile-client/src/components/BookEdit.js
@@ -3,38 +3,8 @@ import React, { Component } from 'react';
 
 
 class BookEdit extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      title: '',
-      author: '',
-      notes: '',
-      status: '',
-      img_url: ''
-    }
-  }
-
-  handleOnChange = event => {
-    const { name, value } = event.target;
-    const currentBookFormData = Object.assign({}, this.props.bookFormData, {
-      [name]: value
-    })
-    this.props.updateBookFormData(currentBookFormData)
-  }
-
-  handleOnSubmit = event => {
-    event.preventDefault()
-    this.props.createBook(this.props.bookFormData)
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Display single book</h1>
-      </div>
-    )
-  }
+  
 }
+
 
  export default (BookEdit);
